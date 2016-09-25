981ac8cb26f2cd5c6f9079d2ac96450480c65e32
phre4k <me@phre4k.at>
2016-09-25 14:56:22 +0200

    add more steam libs to delete

--- delete-steamlibs.sh
+++ delete-steamlibs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-find ~/.local/share/Steam/ \( -name "libgcc_s.so*" -o -name "libstdc++.so*" -o -name "libxcb.so*" \) -print -delete
+find ~/.local/share/Steam/ \( -name "libgcc_s.so*" -o -name "libstdc++.so*" -o -name "libxcb.so*" -o -name "libgpg-error.so*" -o -name "libX11.so*" \) -print -delete
